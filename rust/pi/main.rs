a6d1098058c471e0d57d0174f22dd8a08403fc9f
Eliiia <grzesiul1995@gmail.com>
2021-11-30 22:12:15 +0000

    basic rust programs

---
+++ rust/pi/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let pi = 3.141592;
+
+    // add : to modify it, i think?
+    // add . to cut it, assuming its a number
+    // add a number (in this case 3) or a * or some other stuff 
+    println!("pi is roughly {:.3}", pi);
+    
+
+    
+}
