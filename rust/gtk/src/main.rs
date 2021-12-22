5a754f5a5418813c73a9416ca90483be53820a48
Eliiia <grzesiul1995@gmail.com>
2021-12-22 22:41:14 +0000

    ..Changed GTK example..

--- rust/gtk/src/main.rs
+++ rust/gtk/src/main.rs
@@ -1,28 +1,26 @@
-use gtk;
+use glib::clone;
+// glib and other dependencies are re-exported by the gtk crate
+use gtk::glib;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button};
+
+// When the application is launched…
+fn on_activate(application: &gtk::Application) {
+    // … create a new window …
+    let window = gtk::ApplicationWindow::new(application);
+    // … with a button in it …
+    let button = gtk::Button::with_label("Quit program");
+    // … which closes the window when clicked
+    button.connect_clicked(clone!(@weak window => move |_| window.close()));
+    window.set_child(Some(&button));
+    window.present();
+}
 
 fn main() {
-    let application = Application::builder()
-        .application_id("com.example.FirstGtkApp")
+    // Create a new application with the builder pattern
+    let app = gtk::Application::builder()
+        .application_id("com.github.gtk-rs.examples.basic")
         .build();
-
-    application.connect_activate(|app| {
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .title("First GTK Program")
-            .default_width(350)
-            .default_height(70)
-            .build();
-
-        let button = Button::with_label("Click me!");
-        button.connect_clicked(|_| {
-            eprintln!("Clicked!");
-        });
-        window.set_child(Some(&button));
-
-        window.show();
-    });
-
-    application.run();
+    app.connect_activate(on_activate);
+    // Run the application
+    app.run();
 }
