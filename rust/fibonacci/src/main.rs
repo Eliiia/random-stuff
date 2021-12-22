97e5ad0fbeb9be11eef479900f533c447cc496b2
Eliiia <grzesiul1995@gmail.com>
2021-12-22 15:11:14 +0000

    use vectors (instead of arrays) in `rust/fibonacci`

--- rust/fibonacci/src/main.rs
+++ rust/fibonacci/src/main.rs
@@ -1,12 +1,14 @@
 fn main() {
-    const L:usize = 20; // length
+    const L: usize = 20; // length
 
-    let mut arr = [0;L]; //sidenote from later: apparently i should have used a vector, not an array
-    arr[1] = 1;
+    let mut arr = vec![0, 1];
+    // alternate method of vectoring:
+    /*let mut arr: Vec<u32> = Vec::new();
+    arr.push(0);
+    arr.push(1);*/
 
     for x in 2..L {
-        arr[x] = arr[x-1]+arr[x-2];
-        //println!("{}", x);
+        arr.push(arr[x - 1] + arr[x - 2])
     }
 
     for x in arr {
