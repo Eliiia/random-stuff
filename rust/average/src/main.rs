7211364cea04584a0eef6b8a74fe9c57dd2fd66a
Eliiia <grzesiul1995@gmail.com>
2021-12-30 15:22:22 +0000

    actually finish average 😎

--- rust/average/src/main.rs
+++ rust/average/src/main.rs
@@ -1,7 +1,8 @@
 use std::io;
+use std::collections::HashMap;
 
 fn main() {
-    let mut numbers: Vec<f32> = Vec::new();
+    let mut numbers: Vec<u32> = Vec::new();
 
     loop {
         println!("Input a number, or 'quit':");
@@ -16,7 +17,7 @@ fn main() {
             break;
         };
 
-        let inp: f32 = match inp.trim().parse() {
+        let inp: u32 = match inp.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
@@ -25,11 +26,36 @@ fn main() {
     }
 
     // average:
-    let mut total: f32 = 0.0;
-    for x in &numbers {
-        total += x;
-    }
+    let average = {
+        let mut total: u32 = 0;
+        for x in &numbers {
+            total += x;
+        }
 
-    let average = total / numbers.len() as f32;
+        total as f32 / numbers.len() as f32
+    };
     println!("Average: {}", average);
+
+    let median = {
+        numbers.sort_by(|a, b| a.partial_cmp(b).unwrap()); // do not understand how this works but whatever this is my fault im using f32
+        
+        numbers[numbers.len()/2]
+    };
+    println!("Median: {}", median);
+
+    let mode = {
+        let mut map = HashMap::new();
+        for n in numbers {
+            let count = map.entry(n).or_insert(0);
+            *count += 1; // .or_insert() returns a reference to the value
+        };
+        let mut highest = (0,0);
+        for x in map {
+            if x.1 > highest.1 {
+                highest = (x.0, x.1);
+            }
+        };
+        highest.0
+    };
+    println!("Mode: {:?}", mode);
 }
