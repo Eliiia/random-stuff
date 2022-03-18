fec488c46a546067df56dae8c67409c256c2eb20
Eliiia <grzesiul1995@gmail.com>
2022-03-18 22:34:17 +0000

    fizzbuzz C# omg

---
+++ cs/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to fizzbuzz or something");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                string outp = i.ToString();
+
+                if (i % 3 == 0 && i % 5 == 0) outp = "FizzBuzz";
+                else if (i % 3 == 0) outp = "Fizz";
+                else if (i % 5 == 0) outp = "Buzz";
+
+                if (i%25 == 0)
+                {
+                    Console.WriteLine(outp);
+                }
+                else
+                {
+                    Console.Write(outp + " ");
+                }
+            }
+        }
+    }
+}
