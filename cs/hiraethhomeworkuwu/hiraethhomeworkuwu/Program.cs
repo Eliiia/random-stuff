b272567ab6dd7f82f53c7dc6214dd5b3a5d6ec96
Eliiia <grzesiul1995@gmail.com>
2022-03-19 13:43:54 +0000

    hiraethhomeworkuwu

---
+++ cs/hiraethhomeworkuwu/hiraethhomeworkuwu/Program.cs
@@ -0,0 +1,26 @@
+﻿
+if (args[0].Length >= 10)
+{
+    string x = "";
+    
+    for (int i=0; i<args[0].Length; i++)
+    {
+        if (i > 10)
+        {
+            x += Char.ToUpper(args[0][i]);
+        }
+        else x += args[0][i];
+    }
+    
+    Console.WriteLine(x);
+}
+else
+{
+    char[] arr = args[0].ToCharArray();
+    arr[0] = '$';
+    arr[^1] = '@';
+
+    string x = new string(arr);
+    
+    Console.WriteLine(x);
+}
