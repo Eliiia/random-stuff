0c4f7a0200be17bf38ee1a1b997988865896b034
Elia <eliiia@protonmail.com>
2022-07-07 13:16:00 +0100

    omg a c# bot im so smart

---
+++ cs/bot1/bot1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    public async Task MainAsync()
+    {
+        var client = new DiscordSocketClient();
+        client.Log += Log;
+
+        string token = Environment.GetEnvironmentVariable("TOKEN");
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
